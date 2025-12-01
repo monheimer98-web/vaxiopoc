e7a306c5bec416e5ca45a841e11852bfba661568
Nicki <monheimer98@gmail.com>
2025-12-01 22:04:47 +0100

    Set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Wichtig: base muss auf den Repository-Namen zeigen -> /vaxiopoc/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/vaxiopoc/"
+});
